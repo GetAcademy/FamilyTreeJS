cc9bf9a6abbd848687a15819ddad62909914be8d
GET Academy <terje@kolderup.net>
2023-11-07 11:01:33 +0100

    x

--- js/view2.js
+++ js/view2.js
@@ -1,13 +1,21 @@
 function updateView() {
-    const id = model.selectedPersonId;
+    document.getElementById('app').innerHTML =
+        createFullHtmlForPerson(8);
+}
+function createFullHtmlForPerson(id) {
     const person = findPersonById(id);
-    document.getElementById('app').innerHTML = /*HTML*/`
-        ${createHtmlForPerson(id, '', 'h3', false)}
-        ${createHtmlForPerson(person.mother, 'Mor:', 'p', true)}
-        ${createHtmlForPerson(person.father, 'Far:', 'p', true)}  
-        <h3>Barn</h3>      
-        ${createHtmlForChildren(id)}        
-    `;
+    let html = createHtmlForPerson(id, '', 'h3', false)
+        + createHtmlForPerson(person.mother, 'Mor:', 'p', true)
+        + createHtmlForPerson(person.father, 'Far:', 'p', true);
+    const children = findChildren(id);
+    if (!children.length){
+        html += "<h3>Barn</h3>Ingen barn";
+        return html;
+    } 
+    for (let child of children) {
+        html += createFullHtmlForPerson(child.id);
+    }
+    return html;
 }
 
 function findChildren(id) {
@@ -28,16 +36,17 @@ function createHtmlForChildren(id) {
     }
     return html;
 }
+
 function createHtmlForPerson(personId, label, tag, showButton) {
     const person = findPersonById(personId);
     if (person == null) return '';
-    const btnHtml = !showButton ? '' : `<button onclick="selectPerson(${person.id})">vis</button>`;
+    const btnHtml = !showButton ? '' : `<button onclick = "selectPerson(${person.id})" > vis</button > `;
     return/*HTML*/`
         <${tag}>
             ${label} ${person.firstName} - Født: ${person.birthYear}
             ${btnHtml}
-        </${tag}>    
-    `;
+        </${tag}>
+        `;
 }
 
 function findPersonById(id) {
